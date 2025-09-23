81ea2e5f2368608aa765ecfbf83ced9acfce67da
abby <abigailrodrigo30@gmail.com>
2025-09-23 11:52:29 +0800

    I changed the add() function by adding a variable ' insertIndex ' and set it to -1 at the start. This helps the program find where to put the new entry in the list. The program checks each entry already in the list and use ' compare() ' to see if the new entry should come before any of them. If it should, the program remembers that spot by setting ' insertIndex ' to that position. If the new entry doesn’t come before any existing entries, ' insertIndex ' stays -1. Then the program sets ' insertIndex ' to the end of the list so the new entry can be added there, if there is space. But if the list is full and the new entry doesn’t belong anywhere in the list, it shows a message saying the score is too low.

--- src/boardarray.h
+++ src/boardarray.h
@@ -13,38 +13,45 @@ class BoardArray : public Board {
             index = 0;
         }
 
-        void add(Entry* entry) {
-            int pos = index; 
 
-            for(int i = 0; i < index; ++i){ 
-                if(entry->compare(&array[i])){ 
-                    pos = i; 
-                    break; 
-                } 
+
+    void add(Entry* entry) {
+            int insertIndex = -1;
+
+            for (int i = 0; i < index; ++i) {
+                if (entry->compare(&array[i])) {
+                    insertIndex = i;
+                    break;
+                }
             }
 
-            if (index < SIZE) { 
-                for (int j = index; j > pos; --j){ 
-                    array[j] = array[j - 1]; 
-                } 
-                array[pos] = *entry; 
-                ++index; 
-            } else { 
-                if (pos == SIZE){ 
-                    std::cout << entry->name << "'s score is too low to be added!" << endl; 
-                } else { 
-                    for(int j = SIZE - 1; j > pos; --j){ 
-                        array[j] = array[j - 1]; 
-                    } 
-                    array[pos] = *entry; 
-                } 
+            if (insertIndex == -1) {
+                insertIndex = index;
             }
 
-            return;
-        };
+            if (index < SIZE) {
+                for (int j = index; j > insertIndex; --j) {
+                    array[j] = array[j - 1];
+                }
+                array[insertIndex] = *entry;
+                ++index;
+            } else {
+                if (insertIndex == SIZE) {
+                    std::cout << entry->name << "'s score is too low to be added!" << std::endl;
+                } else {
+                    for (int j = SIZE - 1; j > insertIndex; --j) {
+                        array[j] = array[j - 1];
+                    }
+                    array[insertIndex] = *entry;
+                }
+            }
+        }
+
 
         // TODO: IMPLEMENT THIS FUNCTION
         // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+
+
 
         void print() {
             for (int i = 0; i < index; i++) {
